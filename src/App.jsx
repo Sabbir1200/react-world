c39968d7a18afe2bb1ebade0581c947149a2905a
4mahbubur4 <mahbubur.mr4@gmail.com>
2025-10-01 22:38:07 +0600

    Learning react world

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Countries from './components/countries/Countries'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+   
+   
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+            <h1>
+                In the Countries 
+            </h1>
+        </div>
+    );
+};
+
+export default countries;
